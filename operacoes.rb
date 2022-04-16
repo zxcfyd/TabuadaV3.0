8efdf1a762c09a1f54e00d721a00c208ed63f74d
zxcfyd <zhao.zxc@outlook.com>
2022-04-16 04:19:53 -0300

    v3.1.1

--- operacoes.rb
+++ operacoes.rb
@@ -3,32 +3,32 @@ class Operacao #Operações Matemáticas
   def somar(numero) #Somar
     valor = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     valor.each do |soma|
-      @resultado = numero + soma
-      puts "#{numero} + #{soma} = #{@resultado}"
+      resultado = numero + soma
+      puts "#{numero} + #{soma} = #{resultado}"
     end
   end
 
   def subtrair(numero) #Subtrair
     valor = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     valor.each do |subtrair|
-      @resultado = numero - subtrair
-      puts "#{numero} - #{subtrair} = #{@resultado}"
+      resultado = numero - subtrair
+      puts "#{numero} - #{subtrair} = #{resultado}"
     end
   end
 
   def multiplicar(numero) #Multiplicar
     valor = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     valor.each do |multiplicar|
-      @resultado = numero * multiplicar
-      puts "#{numero} x #{multiplicar} = #{@resultado}"
+      resultado = numero * multiplicar
+      puts "#{numero} x #{multiplicar} = #{resultado}"
     end
   end
 
   def dividir(numero) #Dividir
     valor = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     valor.each do |dividir|
-      @resultado = numero / dividir
-      puts "#{numero} / #{dividir} = #{@resultado}"
+      resultado = numero / dividir
+      puts "#{numero} / #{dividir} = #{resultado}"
     end
   end
 end
